b909b40f337dab2fbe0a6e3746cb985a632794af
waynemamahit <wayne.mamahit@gmail.com>
2023-06-01 16:11:22 +0700

    Change name props AspectListProps

--- src/components/AspectList.tsx
+++ src/components/AspectList.tsx
@@ -1,10 +1,10 @@
 import { ReactElement, memo } from 'react';
 
-export interface AspectList {
+export interface AspectListProps {
   count: number;
   render: (i: number) => ReactElement;
 }
-export function AspectList({ count, render }: AspectList) {
+export function AspectList({ count, render }: AspectListProps) {
   const aspectItems = [];
   for (let i = 1; i <= count; i++) {
     aspectItems.push(<span key={i}>{render(i)}</span>);
